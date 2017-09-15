f16378fe51b1edd7a61f618cfb1dfcc7dbc49f3f
Luke Miles <rashreportloaf@gmail.com>
2017-09-14 20:51:38 -0400

    explained and expanded source code; moved from python2 to python3

--- rhyme.py
+++ rhyme.py
@@ -1,78 +1,86 @@
-# Luke Miles, January 2015
+# Luke Miles, September 2017
+# Python 3
+# TODO: update the other files in this directory
+
 import random
-stresses = {'AA1', 'AE1', 'AH1', 'AO1', 'AW1', 'AY1', 'EH1', 'ER1', 'EY1', 'IH1', 'IY1', 'OW1', 'OY1', 'UH1', 'UW1'}
 
-with open("phodict.txt") as fille:
-    phodict = dict()
-    for line in fille:
-        words = line.split()
-        phodict[words[0]] = tuple(words[1:])
+STRESSES = {'AA1', 'AE1', 'AH1', 'AO1', 'AW1', 'AY1', 'EH1', 'ER1', 'EY1', 'IH1', 'IY1', 'OW1', 'OY1', 'UH1', 'UW1'} # set of all stressed vowel sounds
 
-def sle(lst, sublst):
-    """ sublistexists """
-    n = len(sublst)
-    return any((sublst == lst[i:i+n]) for i in xrange(len(lst)-n+1))
+def fileToDict(filepath):
+    dictA = dict()
+    with open(filepath) as fileA:
+        for line in fileA:
+            words = line.split() # splits the line on any whitespace
+            dictA[words[0]] = tuple(words[1:])
+    return dictA
 
-def dr(w1, w2):
-    """ doesrhyme """
-    p1, p2 = phodict[wq], phodict[w2]
-    if sle(p1, p2) or sle(p2, p1):
+PHODICT = fileToDict("phodict.txt") # phonetic dictionary
+
+def isSubList(listA, listB):
+    "returns whether or not one list contains the other"
+    if len(listA) < len(listB):
+        return isSubList(listB, listA)
+    n = len(listB)
+    for start in range(len(listA)-n+1):
+        if all(listA[start+i] == listB[i] for i in range(n)):
+            return True
+    return False
+
+def isRhyme(wordA, wordB):
+    "returns whether or not word1 and word2 rhyme"
+    soundsA, soundsB = PHODICT[wordA], PHODICT[wordB]
+    if isSubList(soundsA, soundsB):
+        # you don't want pickle to rhyme with superpickle
         return False
-    for ind,sound in enumerate(reversed(p1)):
-        if sound in stresses:
+    for index,sound in enumerate(reversed(soundsA)):
+        if sound in STRESSES:
             break
-    return p1[-ind-1:] == p2[-ind-1:]
+    return p1[-index-1:] == p2[-index-1:]
 
-def hms(w):
-    """ how many syllables """
-    p = phodict[w]
-    return sum((sound[0] in "AEIOU") for sound in p)
+def syllableCount(word):
+    "returns the number of syllables in word"
+    sounds = PHODICT[word]
+    return sum(int(sound[0] in "AEIOU") for sound in sounds)
 
-def msm():
-    """ make syllable map """
-    dictt = {i:set() for i in xrange(0,15)}
-    for word in phodict.iterkeys():
-        dictt[hms(word)].add(word)
-    return dictt
+def makeSyllableMap():
+    """returns a dictionary where a key is a number of syllables and its value
+    is the set of all words with that many syllables"""
+    dictA = {i: set() for i in range(0,15)}
+    for word in PHODICT.keys():
+        dictA[syllableCount(word)].add(word)
+    return dictA
 
-syldict = msm()
+SYLDICT = makeSyllableMap()
 
-def gr(w):
-    """ get rhymes """
-    p = phodict[w]
-    for ind,sound in enumerate(reversed(p)):
-        if sound in stresses:
-            p_end = p[-ind-1:]
+def getRhymes(word):
+    "yields all words that rhyme with word"
+    sounds = PHODICT[word]
+    for index,sound in enumerate(reversed(sounds)):
+        if sound in STRESSES:
+            ending = sounds[-index-1:]
             break
     yielded = set()
-    for w2 in phodict.iterkeys():
-        p2 = phodict[w2]
-        if p_end != p2[-ind-1:]:
-            continue
-        if sle(p, p2) or sle(p2, p):
-            continue
-        if p2 in yielded:
-            continue
-        yielded.add(p2)
-        yield w2
+    for wordB, soundsB in PHODICT.items():
+        if (ending == soundsB[-index-1:]) and (soundsB not in yielded) and (not isSubList(sounds, soundsB)):
+            yielded.add(soundsB)
+            yield wordB
 
-def gnsw(n):
-    """ grab n syllable word """
-    return random.sample(syldict[n], 1)[0]
+def findWord(n):
+    "returns a random n syllable word"
+    return random.sample(SYLDICT[n], 1)[0]
 
-def limerick():
-    """ 10A,10A,6B,6B,10A """
-    e1 = gnsw(1) # ending 1
-    r1 = tuple(gr(e1))
+def makeLimerick():
+    "rhyme pattern: 10A,10A,6B,6B,10A "
+    e1 = findWord(1) # e1 is short for ending 1
+    r1 = tuple(getRhymes(e1))
     e2 = random.choice(r1)
     e5 = random.choice(r1)
-    e3 = gnsw(1)
-    r3 = tuple(gr(e3))
+    e3 = findWord(1)
+    r3 = tuple(getRhymes(e3))
     e4 = random.choice(r3)
-    l1 = ' '.join(gnsw(4) for _ in xrange(2)) + ' ' + e1
-    l2 = ' '.join(gnsw(4) for _ in xrange(2)) + ' ' + e2
-    l5 = ' '.join(gnsw(4) for _ in xrange(2)) + ' ' + e5
-    l3 = ' '.join(gnsw(3) for _ in xrange(1)) + ' ' + e3
-    l4 = ' '.join(gnsw(3) for _ in xrange(1)) + ' ' + e4
-    return '\n'.join((l1,l2,l3,l4,l5))
-
+    lines = [[findWord(4), findWord(4), e1],
+             [findWord(4), findWord(4), e2],
+             [findWord(3), e3],
+             [findWord(3), e4],
+             [findWord(4), findWord(4), e5]]
+    return '\n'.join(' '.join(line) for line in lines)
